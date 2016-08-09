e80dab5c5b89a25cfc1fc681f92ce06c4059825b
ederg <Gerald.Eder@ymail.com>
2016-08-09 14:40:22 +0200

    Add files via upload

---
+++ ComponentApp/WebContent/i2s_pm_ess/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+  "com/inn2soft/pm/ess/controller/BaseController"
+], function (BaseController) {
+  "use strict";
+
+  return BaseController.extend("com.inn2soft.pm.ess.controller.App", {
+
+    onInit: function () {
+
+    }
+
+  });
+
+});
